63171e4eede730d72c636da0b2c30c9af4ab37e7
Antonio <antonio18casta@aol.com>
2023-07-17 17:07:18 -0400

    commit tree 656ab3939e8aa1f72cffbcd1cb92d262bd06092b

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+  # Module whose methods to be used in another class as instance methods, then must include the module.
+  # Module's methods to be used in another class as class methods, you must extend the module.
+
+  # include Dance
+  # extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
